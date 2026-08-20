c80df5272a04d4ffda34b80121ec5ad64c853b96
mohammedusman <isa.nuh.usman@gmail,com>
2026-08-20 17:46:42 +0530

    Slicing

---
+++ slicing.py
@@ -0,0 +1,10 @@
+numbers = [10,20,30,40,50,60,70,80,90,100]
+print(numbers[1:5])
+print(numbers[8:1])
+print(numbers[2:5:2])
+print(numbers[-5:-1])
+print(numbers[3::-1])
+print(numbers[6:1:-1])
+print(numbers[::])
+print(numbers[::2])
+print(numbers[::-1])
