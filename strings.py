86fb60b625e0c591bfba869e45743207f0b72b4a
mohammedusman <isa.nuh.usman@gmail,com>
2026-08-23 16:35:14 +0530

    .

--- strings.py
+++ strings.py
@@ -20,6 +20,6 @@ word = input("Enter A Word:")
 reverse = word[::-1]
 
 if word == reverse:
-    print(word,"Is Palindrome")
+    print(word,"Is Palindrome" )
 else:
-    print(word,"Is Not Palindrome")
+    print(word,"Is Not Palindrome" )
